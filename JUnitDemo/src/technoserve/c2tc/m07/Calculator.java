247307e8a79bbbaa4e69785ab834c09eadacb35e
Abhie2809 <jagdaleabhijeet14@gmail.com>
2022-03-21 00:19:19 +0530

    JUnit Demo

---
+++ JUnitDemo/src/technoserve/c2tc/m07/Calculator.java
@@ -0,0 +1,12 @@
+package technoserve.c2tc.m07;
+
+public class Calculator {
+	int add(int num1 , int num2)
+	{
+	return num1 + num2;
+	}
+	double area(int radius) 
+	{
+		return radius*radius*Math.PI;
+	}
+}
