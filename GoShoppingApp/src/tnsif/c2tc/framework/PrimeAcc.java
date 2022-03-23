c7569c737a7b9b72887b5868e6f63a407d814782
Abhie2809 <jagdaleabhijeet14@gmail.com>
2022-03-23 21:54:09 +0530

    Ecommerce Website Example

---
+++ GoShoppingApp/src/tnsif/c2tc/framework/PrimeAcc.java
@@ -0,0 +1,24 @@
+package tnsif.c2tc.framework;
+
+public class PrimeAcc extends ShopAcc {
+	public PrimeAcc(int accno, String accname, float charges) {
+		super(accno, accname, charges);
+	}
+	
+	private static boolean isprime=true;
+
+	public boolean isIsprime() {
+		return isprime;
+	}
+
+	public void setIsprime(boolean isprime) {
+		this.isprime = isprime;
+	}
+
+	@Override
+	public String toString() {
+		return "PrimeAcc [isprime=" + isprime + ", getAccno()=" + getAccno() + ", getAccname()=" + getAccname()
+				+ ", getCharges()=" + getCharges() + "]";
+	}
+	
+}
